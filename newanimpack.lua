3412e154078e1badc1ddac82eac968ae18c6d398
shlexx <ginirbi@gmail.com>
2024-07-18 13:16:32 +0300

    Create newanimpack.lua

---
+++ newanimpack.lua
@@ -0,0 +1,11 @@
+local Animate = game.Players.LocalPlayer.Character.Animate
+Animate.idle.Animation1.AnimationId = "http://www.roblox.com/asset/?id=18537376492"
+Animate.idle.Animation2.AnimationId = "http://www.roblox.com/asset/?id=18537371272"
+Animate.walk.WalkAnim.AnimationId = "http://www.roblox.com/asset/?id=18537392113"
+Animate.run.RunAnim.AnimationId = "http://www.roblox.com/asset/?id=18537384940"
+Animate.jump.JumpAnim.AnimationId = "http://www.roblox.com/asset/?id=18537380791"
+Animate.fall.FallAnim.AnimationId = "http://www.roblox.com/asset/?id=18537367238"
+Animate.swimidle.SwimIdle.AnimationId = "http://www.roblox.com/asset/?id=18537387180"
+Animate.swim.Swim.AnimationId = "http://www.roblox.com/asset/?id=18537389531"
+Animate.climb.ClimbAnim.AnimationId = "http://www.roblox.com/asset/?id=18537363391"
+game.Players.LocalPlayer.Character.Humanoid.Jump = true
