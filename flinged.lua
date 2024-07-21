fef64a4c69b62ae8b1339091cc2ccf35b2472746
shlexx <ginirbi@gmail.com>
2024-07-21 13:00:26 +0300

    Create flinged.lua

---
+++ flinged.lua
@@ -0,0 +1,4 @@
+local power = 500
+local hrp = game.Players.LocalPlayer.Character.HumanoidRootPart
+hrp.AssemblyLinearVelocity = Vector3.new(power,power,power)
+hrp.AssemblyAngularVelocity = Vector3.new(power,power,power)
