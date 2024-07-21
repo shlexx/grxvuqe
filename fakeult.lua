7a28f9e37046cbbdff0118b789d6c537ffb40724
shlexx <ginirbi@gmail.com>
2024-07-21 13:41:47 +0300

    Create fakeult.lua

---
+++ fakeult.lua
@@ -0,0 +1,8 @@
+local mh = game.Players.LocalPlayer.PlayerGui.ScreenGui.MagicHealth
+
+mh.Health.Bar.Size = UDim2.new(1,0,0,25)
+mh.Health.Bar.ImageTransparency = 0
+mh.Health.Bar.Bar.Size = UDim2.new(0,0,1,-25)
+mh.Health.Bar.Bar.ImageTransparency = 1
+mh.Ult.Visible = true
+mh.Ult.TextLabel.Visible = true
