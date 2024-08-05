523679f1f98159b8d104644a016bd5d9f5b6cd52
shlexx <ginirbi@gmail.com>
2024-08-05 21:40:44 +0300

    Update aar.lua

--- aar.lua
+++ aar.lua
@@ -3,4 +3,5 @@ for _,v in pairs(game:GetDescendants()) do
 		print("game." .. v:GetFullName() .. " [RemoteEvent]")
 	elseif v:IsA("RemoteFunction") then
 		print("game." .. v:GetFullName() .. " [RemoteFunction]")
+	end
 end
