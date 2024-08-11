e1c2eec8b68821bc87ccdb8ae5260017c4db8dc8
shlexx <ginirbi@gmail.com>
2024-08-11 16:49:49 +0300

    Update DestroySegway.lua

--- vCmd/DestroySegway.lua
+++ vCmd/DestroySegway.lua
@@ -879,37 +879,40 @@ UserInputService.InputBegan:Connect(function(input)
 				Destroy(v)
 			end
 		elseif args[1] == defaultprefix .. "btools" then
-			local sb = Instance.new("SelectionBox")
-			local player = Players.LocalPlayer
-			local backpack = player:WaitForChild("Backpack")
-			local mouse = player:GetMouse()
-			local trg = mouse.Target
-
-			local tool = Instance.new("Tool")
-			tool.TextureId = "http://www.roblox.com/asset/?id=10684453"
-			tool.Name = "delete"
-			tool.ToolTip = ":troll:"
-			tool.RequiresHandle = false
-			tool.CanBeDropped = false
-			tool.Parent = backpack
-
-			local function onActivated()
-				if trg and trg:IsA("BasePart") then
-					Destroy(trg)
+			local Players = game:GetService("Players")
+			local mouse = game.Players.LocalPlayer:GetMouse()
+			local selectionbox = Instance.new("SelectionBox", workspace)
+			local equipped = false
+			local oldmouse = mouse.Icon
+			local destroytool = Instance.new("Tool", Players.LocalPlayer:FindFirstChildOfClass("Backpack"))
+			destroytool.RequiresHandle = false
+			destroytool.Name = "Delete"
+			destroytool.TextureId = "http://www.roblox.com/asset/?id=12223874"
+			destroytool.CanBeDropped = false
+			destroytool.Equipped:connect(function()
+				equipped = true
+				mouse.Icon = "rbxasset://textures\\HammerCursor.png"
+				while equipped do
+					selectionbox.Adornee = mouse.Target
+					wait()
 				end
-			end
-			
-			local function onEquipped()
-				if trg and trg:IsA("BasePart") then
-					while wait(0.01) do
-						sb.Parent = trg
-						sb.Adornee = trg
-					end
-				end
-			end
-
-			tool.Activated:Connect(onActivated)
-			tool.Equipped:Connect(onEquipped)
+			end)
+			destroytool.Unequipped:connect(function()
+				equipped = false
+				selectionbox.Adornee = nil
+				mouse.Icon = oldmouse
+				print(oldmouse)
+			end)
+			destroytool.Activated:connect(function()
+				local explosion = Instance.new("Explosion", workspace)
+				explosion.BlastPressure = 0
+				explosion.BlastRadius = 0
+				explosion.DestroyJointRadiusPercent = 0
+				explosion.ExplosionType = Enum.ExplosionType.NoCraters
+				explosion.Position = mouse.Target.Position
+				Destroy(mouse.Target)
+			end)
+			game:GetService("StarterGui"):SetCoreGuiEnabled('Backpack', true)
 		else
 			TextLabel_2.Text = "Invalid command or prefix."
 		end
