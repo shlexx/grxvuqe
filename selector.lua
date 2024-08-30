227bf52e0e513e7131949391e15454fe973bfe22
shlexx <ginirbi@gmail.com>
2024-08-30 22:52:59 +0300

    Update selector.lua

--- selector.lua
+++ selector.lua
@@ -1,3 +1,6 @@
+if game.CoreGui.SelectorGUI then
+    game.CoreGui.SelectorGUI:Destroy()
+end
 local Players = game:GetService("Players")
 local mouse = Players.LocalPlayer:GetMouse()
 local selectionbox = Instance.new("SelectionBox", workspace)
@@ -21,20 +24,23 @@ SelectorText.TextColor3 = Color3.fromRGB(0, 0, 0)
 SelectorText.TextSize = 35.000
 SelectorText.Text = ""
 SelectorText.TextXAlignment = Enum.TextXAlignment.Right
+SelectorText.Visible = false
 selectortool.RequiresHandle = false
 selectortool.Name = "Selector"
 selectortool.TextureId = "rbxasset://textures\\ArrowCursor.png"
 selectortool.CanBeDropped = false
 selectortool.Equipped:Connect(function()
 	equipped = true
+	SelectorText.Visible = true
 	while equipped do
 		selectionbox.Adornee = mouse.Target
 		SelectorText.Text = "game." .. mouse.Target:GetFullName()
-		wait()
+		task.wait()
 	end
 end)
 selectortool.Unequipped:Connect(function()
 	equipped = false
 	selectionbox.Adornee = nil
+	SelectorText.Visible = false
 end)
 game:GetService("StarterGui"):SetCoreGuiEnabled('Backpack', true)
