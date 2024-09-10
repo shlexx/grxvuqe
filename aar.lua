4db6326d697583d109d4ebd8eca9928b191d988d
shlexx <ginirbi@gmail.com>
2024-09-10 16:29:09 +0300

    Update aar.lua

--- aar.lua
+++ aar.lua
@@ -6,3 +6,19 @@ for _,v in pairs(game:GetDescendants()) do
 		print("game." .. v:GetFullName() .. " 🔵")
 	end
 end
+task.wait()
+print('\n[[Added/Removed]]\n➕ - New Remote, ➖ - Removed Remote')
+game.DescendantAdded:Connect(function(v)
+	if v:IsA("RemoteEvent") then
+		print("game." .. v:GetFullName() .. " ➕🟢")
+	elseif v:IsA("RemoteFunction") then
+		print("game." .. v:GetFullName() .. " ➕🔵")
+	end
+end)
+game.DescendantRemoving:Connect(function(v)
+	if v:IsA("RemoteEvent") then
+		print("game." .. v:GetFullName() .. " ➖🟢")
+	elseif v:IsA("RemoteFunction") then
+		print("game." .. v:GetFullName() .. " ➖🔵")
+	end
+end)
