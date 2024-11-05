166e1a3af908171c20fe33d26c707b23b440fa1d
shlexx <ginirbi@gmail.com>
2024-11-05 16:27:22 +0200

    Update dash.lua

--- dash.lua
+++ dash.lua
@@ -1,19 +1,22 @@
 local char = game.Players.LocalPlayer.Character
+local torso = char.Torso or char.UpperTorso
 local uis = game:GetService("UserInputService")
 uis.InputBegan:Connect(function(i,g)
 if not g and i.KeyCode == Enum.KeyCode.Q then
 local bv = Instance.new("BodyVelocity")
-bv.Parent = char.Torso
+bv.Parent = torso
 bv.P = 1250
 bv.MaxForce = Vector3.one*math.huge
 if uis:IsKeyDown(Enum.KeyCode.W) then
-bv.Velocity = char.Torso.CFrame.lookVector*50
-elseif uis:IsKeyDown(Enum.KeyCode.S) then
-bv.Velocity = -char.Torso.CFrame.lookVector*50
+bv.Velocity = torso.CFrame.lookVector*50
 elseif uis:IsKeyDown(Enum.KeyCode.A) then
-bv.Velocity = -char.Torso.CFrame.RightVector*50
+bv.Velocity = -torso.CFrame.RightVector*50
+elseif uis:IsKeyDown(Enum.KeyCode.S) then
+bv.Velocity = -torso.CFrame.lookVector*50
 elseif uis:IsKeyDown(Enum.KeyCode.D) then
-bv.Velocity = char.Torso.CFrame.RightVector*50
+bv.Velocity = torso.CFrame.RightVector*50
+else
+bv.Velocity = torso.CFrame.lookVector*50
 end
 game:GetService("Debris"):AddItem(bv,0.2)
 end
