c823576be31eabd351887e9a62102e9e12f827c1
shlexx <ginirbi@gmail.com>
2024-11-09 10:52:28 +0200

    Create lop.lua

---
+++ lop.lua
@@ -0,0 +1,2 @@
+game:GetService("ReplicatedStorage").Events.PurchaseTroll:InvokeServer({ItemName="ShrinkPlayer",Key="x5",Cash=-9223372036854774000})
+game.Players.LocalPlayer.Character.Humanoid.Health = 0
