9b8ecb389603d07ab4a5eaaeb65dfaf72ebaf970
shlexx <ginirbi@gmail.com>
2024-11-17 21:15:04 +0200

    Create tpose.lua

---
+++ tpose.lua
@@ -0,0 +1,6 @@
+local anim = Instance.new("Animation")
+anim.AnimationId = "rbxassetid://27432691"
+local panim = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(anim)
+panim:Play()
+panim.TimePosition = 1.49
+panim:AdjustSpeed(0)
