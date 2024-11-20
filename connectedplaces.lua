bd7a28f1763d46a8f968e986594929899cde754b
shlexx <ginirbi@gmail.com>
2024-11-20 19:25:01 +0200

    Update connectedplaces.lua

--- connectedplaces.lua
+++ connectedplaces.lua
@@ -1,1 +1,1 @@
-for _,v in pairs(game.AssetService:GetGamePlacesAsync():GetCurrentPage()) do  print(v.PlaceId .. "  -  " .. game:GetService("MarketplaceService"):GetProductInfo(v.PlaceId).Name)  end
+for _,v in pairs(game.AssetService:GetGamePlacesAsync():GetCurrentPage()) do  print(v.PlaceId .. "  -  " .. game:GetService("MarketplaceService"):GetProductInfo(v.PlaceId).Name)  end  print("Current Place: " .. tostring(game.PlaceId))
