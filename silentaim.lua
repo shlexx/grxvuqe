97ade37f4ef0e9d00264a90c2b1a519b0c3d465c
shlexx <ginirbi@gmail.com>
2024-12-09 20:53:33 +0200

    Update silentaim.lua

--- silentaim.lua
+++ silentaim.lua
@@ -1,1 +1,12 @@
-local mouse = game.Players.LocalPlayer:GetMouse();local mouse1click = mouse1click or function() game:GetService("VirtualUser"):ClickButton1(game:GetService("UserInputService"):GetMouseLocation(), p:GetMouse().Hit) end;while task.wait() do;if mouse then;if mouse.Target then;if mouse.Target.Parent:FindFirstChild("Humanoid") then;mouse1click();end;end;end;end
+local mouse = game.Players.LocalPlayer:GetMouse()
+local click = function() game:GetService("VirtualUser"):ClickButton1(game:GetService("UserInputService"):GetMouseLocation(), mouse.Hit) end
+
+while task.wait() do
+    if mouse then
+        if mouse.Target then
+            if mouse.Target.Parent:FindFirstChild("Humanoid") then
+                click()
+            end
+        end
+    end
+end
