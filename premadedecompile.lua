f2301d86b7e43a6254afb5ae43f3b77f4531fba1
shlexx <ginirbi@gmail.com>
2024-12-15 15:16:11 +0200

    Update premadedecompile.lua

--- premadedecompile.lua
+++ premadedecompile.lua
@@ -1,4 +1,4 @@
-ssdecompile = function(v:Script)
+function otherdecompile(v:Script)
 	local source
 	if v.Name == "Vehicle" and v.Parent:FindFirstChild("LocalVehiclePromptGui") then
 		source = [[local Players = game:GetService("Players")
